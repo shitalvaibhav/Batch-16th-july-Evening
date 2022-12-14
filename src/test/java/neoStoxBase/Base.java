571028f52cadbd497e63b27783b487fefefa3a32
unknown <avhadshital8@gmail.com>
2022-12-14 10:32:07 +0530

    hiiiii

--- src/test/java/neoStoxBase/Base.java
+++ src/test/java/neoStoxBase/Base.java
@@ -24,6 +24,9 @@ public class Base {
 		  
 		 // driver.get(UtilityNew.readDataFromPropertyFile("url"));
 		  driver.get("https://neostox.com/");
+		  
+		  
+		  
 		  Reporter.log("launching browser", true);
 		  Thread.sleep(1000);
 
